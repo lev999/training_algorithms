0d17ddcfbc6195326006ab126214f06cb2b9406e
Lev999 <lev.magazinnik@gmail.com>
2015-06-22 17:49:47 -0300

    added clock solution

---
+++ src/book/Clock_angle.java
@@ -0,0 +1,27 @@
+package book;
+
+/**
+ * Created by pc-users on 6/22/2015.
+ */
+public class Clock_angle {
+    public static void main(String[] args) {
+        int hour,minute;
+        hour=12;
+        minute=30;
+        System.out.println(hour+" h"+","+minute+" min");
+        System.out.println("Answer: angle: "+(Math.abs(angleHour(hour,minute)-angleMinute(minute))));
+        System.out.println("Exact solution: "+Math.abs((5.5*minute-30*hour))+"(5.5*minute-30*hour)");
+    }
+
+    private static double angleMinute(int minute) {
+
+        return minute*6;
+    }
+
+    private static double angleHour(int hour, int minute) {
+        return (hour*30)+((double)minute/60)*30;
+    }
+
+
+
+}
